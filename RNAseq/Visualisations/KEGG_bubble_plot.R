54f144b22c8be1533d2a0fb93d6a2a0b996ab8d0
Kinga <zygmuntowicz.kin@gmail.com>
2025-03-31 11:50:35 -0400

    Update and rename Kegg to KEGG_bubble_plot.R

---
+++ RNAseq/Visualisations/KEGG_bubble_plot.R
@@ -0,0 +1,19 @@
+install.packages("ggplot2")
+
+# Load data from a CSV file
+data2 <- read.csv("data.csv")
+
+# Check the data
+head(data2)
+
+# Bubble plot
+ggplot(data2, aes(x = Rich_factor, y = reorder(Pathway_term, -Rich_factor))) +
+  geom_point(aes(size = Gene_number, color = P_Value), alpha = 0.7) +
+  scale_color_gradient(low = "yellow", high = "red", guide = "colour", name = "P-Value") +
+  scale_size_continuous(range = c(2, 12), guide = "none") +
+  labs(
+    x = "Rich Factor", y = "Pathway Term",
+    title = "Bubble Plot of Pathway Enrichment Analysis (New Data)"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "bottom")
