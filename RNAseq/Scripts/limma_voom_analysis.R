2cd9c98525bcef051f13b9580dc0e4592c8cfa65
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:36:13 -0400

    Update limma_voom_analysis.R

--- RNAseq/Scripts/limma_voom_analysis.R
+++ RNAseq/Scripts/limma_voom_analysis.R
@@ -1,21 +1,16 @@
 library(limma)
 library(edgeR)
 
-# Load count data
 counts <- read.table("counts.txt", header=TRUE, row.names=1)
 coldata <- data.frame(condition=c("control", "drought"))  
 
-# Prepare data for voom transformation
 d <- DGEList(counts=counts, group=coldata$condition)
 d <- calcNormFactors(d)
 v <- voom(d, design=model.matrix(~ coldata$condition), plot=TRUE)
 
-# Fit the model and perform eBayes
 fit <- lmFit(v, design=model.matrix(~ coldata$condition))
 fit <- eBayes(fit)
 
-# Obtain results
 res <- topTable(fit, number=Inf)
 
-# Write results to CSV file
 write.csv(res, file="zygmukin_limma_voom_results.csv")
