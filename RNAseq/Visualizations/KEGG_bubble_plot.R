f39437d8bccee76f56886f2da2c26ef058742c87
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:37:07 -0400

    Update KEGG_bubble_plot.R

--- RNAseq/Visualizations/KEGG_bubble_plot.R
+++ RNAseq/Visualizations/KEGG_bubble_plot.R
@@ -1,12 +1,9 @@
 install.packages("ggplot2")
 
-# Load data from a CSV file
-data2 <- read.csv("data.csv")
+data2 <- read.csv("zyg_kegg.csv")
 
-# Check the data
 head(data2)
 
-# Bubble plot
 ggplot(data2, aes(x = Rich_factor, y = reorder(Pathway_term, -Rich_factor))) +
   geom_point(aes(size = Gene_number, color = P_Value), alpha = 0.7) +
   scale_color_gradient(low = "yellow", high = "red", guide = "colour", name = "P-Value") +
