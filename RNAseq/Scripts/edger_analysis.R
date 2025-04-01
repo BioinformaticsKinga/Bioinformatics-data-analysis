5fd70477456ef612d216dca48ba2e60f5913e4e0
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:35:48 -0400

    Update edger_analysis.R

--- RNAseq/Scripts/edger_analysis.R
+++ RNAseq/Scripts/edger_analysis.R
@@ -1,23 +1,17 @@
 library(edgeR)
 
-# Load count data and experimental conditions
 counts <- read.table("counts.txt", header=TRUE, row.names=1)
 coldata <- data.frame(condition=c("control", "drought"))  
 
-# Prepare the data for edgeR
 y <- DGEList(counts=counts, group=coldata$condition)
 y <- calcNormFactors(y)
 
-# Fit the model and perform the statistical test
 design <- model.matrix(~ coldata$condition)
 y <- estimateDisp(y, design)
 fit <- glmQLFit(y, design)
 
-# Perform the test
 qlf <- glmQLFTest(fit)
 
-# Obtain results
 res <- topTags(qlf, n=Inf)
 
-# Write results to CSV
 write.csv(as.data.frame(res), file="zygmukin_edger_results.csv")
