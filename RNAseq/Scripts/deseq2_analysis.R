877726518f6c5dd89ff1df81a9456c4126a638b4
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:34:19 -0400

    Update deseq2_analysis.R

--- RNAseq/Scripts/deseq2_analysis.R
+++ RNAseq/Scripts/deseq2_analysis.R
@@ -2,7 +2,7 @@ library(DESeq2)
 
 # Load count data and sample information
 counts <- read.table("counts.txt", header=TRUE, row.names=1)
-coldata <- data.frame(condition=c("control", "treatment"))  # Replace with actual sample conditions
+coldata <- data.frame(condition=c("control", "drought"))  
 
 # Create DESeqDataSet object
 dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition)
