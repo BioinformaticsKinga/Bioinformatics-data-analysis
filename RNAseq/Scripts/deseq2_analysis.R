8a90accede820ff5ac08ee6cae33291ff0c8ee43
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:35:21 -0400

    Update deseq2_analysis.R

--- RNAseq/Scripts/deseq2_analysis.R
+++ RNAseq/Scripts/deseq2_analysis.R
@@ -1,17 +1,13 @@
 library(DESeq2)
 
-# Load count data and sample information
 counts <- read.table("counts.txt", header=TRUE, row.names=1)
 coldata <- data.frame(condition=c("control", "drought"))  
 
 # Create DESeqDataSet object
 dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition)
 
-# Perform DESeq2 analysis
 dds <- DESeq(dds)
 
-# Obtain results
 res <- results(dds)
 
-# Write results to a CSV file
 write.csv(as.data.frame(res), file="zygmukin_deseq2_results.csv")
